0ba9b0bf24306a22c5cdd22abad03436d1fd250a
Zeryab Alam <zeryabalam272@icloud.com>
2025-05-03 00:45:02 +0200

    Update setup to specify source directly

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ setup(
     name='lib-ml',
     use_scm_version=True,
     setup_requires=['setuptools_scm'],
-    packages=find_packages(),
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
     install_requires=open('requirements.txt').read().splitlines(),
     include_package_data=True,
 )
